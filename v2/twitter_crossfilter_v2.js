59c44950f647f07f07730f287ec581dc731a23fe
yungster <niryungster@gmail.com>
2016-10-27 13:55:40 -0400

    editing layout

--- v2/twitter_crossfilter_v2.js
+++ v2/twitter_crossfilter_v2.js
@@ -80,12 +80,12 @@ d3.csv(window.CrossFilter.config.dataUrl, function (data) {
             return d.value.median; });
             
   dateChart
-    .width(800)
-    .height(130)
-    .margins({top: 20, right: 50, bottom: 20, left: 40})
+    .width(400)
+    .height(210)
+    .margins({top: 20, right: 10, bottom: 40, left: 10})
     .dimension(dateDimension)
     .group(dateGroup)
-    .x(d3.time.scale().domain([new Date(2015, 12, 15), new Date(2016, 9, 30)]))
+    .x(d3.time.scale().domain([new Date(2016, 0, 15), new Date(2016, 9, 30)]))
     .xUnits(d3.time.days)
     .barPadding(0.0)
     .outerPadding(0.00)
@@ -94,7 +94,7 @@ d3.csv(window.CrossFilter.config.dataUrl, function (data) {
     .ordinalColors(['#525252'])
 
   chartParty
-    .width(300)
+    .width(200)
     .height(210)
     .margins({top: 20, left: 10, right: 20, bottom: 40})
     .ordinalColors(['#3690c0','#ef3b2c'])
@@ -104,7 +104,7 @@ d3.csv(window.CrossFilter.config.dataUrl, function (data) {
     .xAxis().ticks(5);
 
   chartMentions 
-    .width(450)
+    .width(400)
     .height(250)
     .x(d3.scale.ordinal())
     .xUnits(dc.units.ordinal)
