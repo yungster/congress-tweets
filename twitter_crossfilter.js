55d157829cd428a13743055b12528ed33e0dabfd
yungster <niryungster@gmail.com>
2016-11-10 23:39:38 -0500

    updating date range

--- twitter_crossfilter.js
+++ twitter_crossfilter.js
@@ -85,7 +85,7 @@ d3.csv(window.CrossFilter.config.dataUrl, function (data) {
     .margins({top: 20, right: 10, bottom: 40, left: 40})
     .dimension(dateDimension)
     .group(dateGroup)
-    .x(d3.time.scale().domain([new Date(2016, 1, 1), new Date(2016, 10, 11)]))
+    .x(d3.time.scale().domain([new Date(2016, 1, 1), new Date(2016, 11, 1)]))
     .xUnits(d3.time.days)
     .barPadding(0.0)
     .outerPadding(0.00)
